9ca6df739b75532b2d7bf86acc63b886bba900aa
John Mendoza <johnmendoza9310@gmail.com>
2021-09-10 17:15:11 -0500

    culminacion de la prueba

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,19 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from "../../services/products.service";
 
 
-//https://assets.compramass.com/products/P000000001442@3x.jpg
-
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+
+
+  
   
 
 products:any=[];
 finalData:any=[]
-//prod:any=[];
 
 imageProduct:string="";
 
@@ -37,60 +37,57 @@ imageProduct:string="";
       
       resp.sort( (a:any,b:any)=>a.ordinal - b.ordinal )
 
-
-
-
       //get name of category and push in new array
       const dataCategories:any=[];
       Object.keys(resp).forEach( key=>{
-        const categoryName= resp[key].name;
-        const categoryId= resp[key].id;
-        dataCategories.push( {nameCategory:categoryName, idCategory:categoryId});
-       
+      const categoryName= resp[key].name;
+      const categoryId= resp[key].id;
+      dataCategories.push( {nameCategory:categoryName, idCategory:categoryId});
 
-
-        //get products for category and sort for ordinal
-        const productCategory:any=[];
-        const productsOfCategory = this.products;
-        Object.keys(productsOfCategory).forEach( key=>{
+      //get products for category and sort for ordinal
+      const productCategory:any=[];
+      const productsOfCategory = this.products;
+      Object.keys(productsOfCategory).forEach( key=>{
           
-          const idCategoryProduct= productsOfCategory[key].product_data.categories[0].category_id;
-          const nameProduct = productsOfCategory[key].product_data.name;
-          const ordinal= productsOfCategory[key].product_data.categories[0].ordinal;
-          const price = productsOfCategory[key].product_data.price;
-          const ean = productsOfCategory[key].product_data.ean;
-          const kind = productsOfCategory[key].product_data.kind;
+      const nameProduct = productsOfCategory[key].product_data.name;
+      const ordinal= productsOfCategory[key].product_data.categories[0].ordinal;
+      const kind = productsOfCategory[key].product_data.kind;
+      const idCategoryProduct= productsOfCategory[key].product_data.categories[0].category_id;
+      const price = productsOfCategory[key].product_data.price;
+      const ean = productsOfCategory[key].product_data.ean;
           let image:string="";
 
 
+let img:string;
 
-        // this.getImage(ean,kind);
+if (kind===1) {
+  
+img=`http://assets.compramass.com/products/${ean}-80@3x.jpg`
+  
+}else{
 
-          //image=this.imageProduct;
+  img=`http://assets.compramass.com/products/${ean}@3x.jpg`
 
-
-
-          
-          if (categoryId===idCategoryProduct) {
-            productCategory.push({idCategoryProduct,nameProduct,ordinal,price, ean,kind})
+}
+              
+  if (categoryId===idCategoryProduct) {
+        productCategory.push({idCategoryProduct,nameProduct,ordinal,price, ean,kind, img})
           }
 
           productCategory.sort( (a:any,b:any)=>a.ordinal - b.ordinal );
   
-  
         })
-
-
-
-
         
         this.finalData.push({categoryId,categoryName,productCategory});
-        console.log("DATOS FINAL", this.finalData);
+        
         
 
       })
 
     })
+
+
+    console.log("Datos procesados", this.finalData);
   }
 
 
@@ -102,14 +99,6 @@ imageProduct:string="";
     } )
   }
 
-  getImage(ean:any, kind:any){
-
-    this.productService.getImages(ean,kind)
-    .subscribe( (resp:any)=>{
-      console.log("IMPRIMIENDO IMAGEN ",resp);
-      
-    })
-  }
 
   
 
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -9,16 +9,9 @@ import { map } from "rxjs/operators";
 export class ProductsService {
 
   private url = `http://www.mocky.io/v2/5ed0b4443500005b00ff9e02`;
-  private urlImages=`https://assets.compramass.com/products/`;
-  //private urlImages=`/products/`;
- // private url = `/v2/5ed0b4443500005b00ff9e02`;
-
   constructor( private http: HttpClient) {
-
-    console.log("prueba");
     
    }
-
 
 
   getCategories(){
@@ -26,8 +19,7 @@ export class ProductsService {
     return this.http.get(this.url)
     .pipe( map( (data:any)=>{
 
-      console.log("data completa", data);
-      //console.log("data categorias", data.categories);
+      console.log("Data sin procesar", data);
       return data.categories;
       
     }))
@@ -41,39 +33,10 @@ export class ProductsService {
 
     return this.http.get(this.url)
     .pipe( map( (data:any)=>{
-
-      //console.log("data productos", data.products);
       return data.products;
       
     }))
     
   }
 
-
-  getImages(ean:any, kind:any){
-
-    if (kind ===1) {
-
-      //Get image Kind 1 small image
-    return this.http.get(`${this.urlImages}${ean}-80@3x.jpg`)
-    .pipe( map ( data=>{
-      return data;
-    }))
-      
-    }else {
-
-    //Get image Kind 2 small image
-    return this.http.get(`${this.urlImages}${ean}@3x.jpg`)
-    .pipe( map ( data=>{
-    return data;
-    }))
-      
-    }
-
-
-
-    
-
-
-  }
 }
